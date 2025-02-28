59cf379e834f3a34ca17df6ea9c2822f9933e02d
munasib03 <146694328+munasib03@users.noreply.github.com>
2025-02-28 00:28:18 -0600

    Add files via upload

---
+++ ALU/addsub_4bit.v
@@ -0,0 +1,32 @@
+`timescale 1ns/100ps
+module addsub_4bit (Sum, Ovfl, A, B, sub);
+	
+	input [3:0] A, B; //Input values
+	input sub; // add-sub indicator
+	output [3:0] Sum; //sum output
+	output Ovfl; //To indicate overflow
+	
+	// Internal signals
+	wire Cin_bit0;
+	wire Cout_bit0, Cout_bit1, Cout_bit2, Cout_bit3;
+	wire [3:0] B_operand;
+	
+	// If doing subtraction operation, Carry In of bit 0 needs to be 1
+	assign Cin_bit0 = sub ? 1 : 0;
+	
+	// Invert the B operand if subtraction operation
+	assign B_operand = sub ? ~B : B;
+	
+	// Instantiate full adders and interconnect signals
+	full_adder_1bit FA0(.A(A[0]), .B(B_operand[0]), .Cin(Cin_bit0), .Sum(Sum[0]), .Cout(Cout_bit0));
+	full_adder_1bit FA1(.A(A[1]), .B(B_operand[1]), .Cin(Cout_bit0), .Sum(Sum[1]), .Cout(Cout_bit1));
+	full_adder_1bit FA2(.A(A[2]), .B(B_operand[2]), .Cin(Cout_bit1), .Sum(Sum[2]), .Cout(Cout_bit2));
+	full_adder_1bit FA3(.A(A[3]), .B(B_operand[3]), .Cin(Cout_bit2), .Sum(Sum[3]), .Cout(Cout_bit3));
+	
+	// Check for overflow
+	// Overflow cases: 1) If positve + positve = negative
+	//				   2) If negative + negative = positve
+	
+	assign Ovfl = (~A[3] & ~B_operand[3] & Sum[3]) ? 1 : (A[3] & B_operand[3] & ~Sum[3]) ? 1 : 0;
+
+endmodule
---
+++ ALU/paddsb.v
@@ -0,0 +1,31 @@
+`timescale 1ns/100ps
+module PADDSB (Sum, A, B);
+
+	input [15:0] A, B; // Input data values
+	output [15:0] Sum; // Sum output
+
+	// Internal overflow signals for each 4-bit adders
+	wire Ovfl0, Ovfl1, Ovfl2, Ovfl3;
+	
+	// Wire to connect to subtract pins of 4-bit adders
+	wire sub;
+	
+	// We never subtract so set subtract signal to 0
+	assign sub = 0;
+	
+	// 16-bit wire to drive result of each sub-adders
+	wire [15:0] result;
+	
+	// Instantiate 4-bit adder blocks
+	addsub_4bit iAS0(.A(A[3:0]), .B(B[3:0]), .Sum(result[3:0]), .Ovfl(Ovfl0), .sub(sub));
+	addsub_4bit iAS1(.A(A[7:4]), .B(B[7:4]), .Sum(result[7:4]), .Ovfl(Ovfl1), .sub(sub));
+	addsub_4bit iAS2(.A(A[11:8]), .B(B[11:8]), .Sum(result[11:8]), .Ovfl(Ovfl2), .sub(sub));
+	addsub_4bit iAS3(.A(A[15:12]), .B(B[15:12]), .Sum(result[15:12]), .Ovfl(Ovfl3), .sub(sub));
+	
+	// Check for overflow in each half byte and saturate accordingly
+	assign Sum[3:0] = (Ovfl0 & A[3] & B[3]) ? 4'b1000 : (Ovfl0 & ~A[3] & ~B[3]) ? 4'b0111 : result[3:0];
+	assign Sum[7:4] = (Ovfl1 & A[7] & B[7]) ? 4'b1000 : (Ovfl1 & ~A[7] & ~B[7]) ? 4'b0111 : result[7:4];
+	assign Sum[11:8] = (Ovfl2 & A[11] & B[11]) ? 4'b1000 : (Ovfl2 & ~A[11] & ~B[8]) ? 4'b0111 : result[11:8];
+	assign Sum[15:12] = (Ovfl3 & A[15] & B[15]) ? 4'b1000 : (Ovfl3 & ~A[15] & ~B[15]) ? 4'b0111 : result[7:4];
+
+endmodule
