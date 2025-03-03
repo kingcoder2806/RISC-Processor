0a54849dd7eb3e31a3d57860b5aa9192c94ad9cb
munasib03 <146694328+munasib03@users.noreply.github.com>
2025-03-03 10:51:22 -0600

    fixed PC Incrementer

--- top_level/adder_pc.v
+++ top_level/adder_pc.v
@@ -5,6 +5,11 @@ module adder_pc(A, B, Sub, Sum);
 	input Sub; // not used
 	input [15:0] Sum;
 	
+	// Internal signals
+	wire C[4:0];
+	wire Gg[3:0];
+	wire Pg[3:0];
+	
 	// Assign carry in for each CLA (Look Ahead Block)
 	assign C[0] = 1'b0;
 	assign C[1] = Gg[0] | (Pg[0] & C[0]);
@@ -13,9 +18,9 @@ module adder_pc(A, B, Sub, Sum);
 	assign C[4] = Gg[3] | (Pg[3] & C[3]);
 	
 	// Instantiate 4-bit carry look ahead adders
-	CLA iCLA0(.A(A[3:0]), .B(B_in[3:0]), .Cin(C[0]), .Cout(), .Sum(Unsat_Sum[3:0]), .Gg(Gg[0]), .Pg(Pg[0]));
-	CLA iCLA1(.A(A[7:4]), .B(B_in[7:4]), .Cin(C[1]), .Cout(), .Sum(Unsat_Sum[7:4]), .Gg(Gg[1]), .Pg(Pg[1]));
-	CLA iCLA2(.A(A[11:8]), .B(B_in[11:8]), .Cin(C[2]), .Cout(), .Sum(Unsat_Sum[11:8]), .Gg(Gg[2]), .Pg(Pg[2]));
-	CLA iCLA3(.A(A[15:12]), .B(B_in[15:12]), .Cin(C[3]), .Cout(), .Sum(Unsat_Sum[15:12]), .Gg(Gg[3]), .Pg(Pg[3]));
+	CLA iCLA0(.A(A[3:0]), .B(B[3:0]), .Cin(C[0]), .Cout(), .Sum(Sum[3:0]), .Gg(Gg[0]), .Pg(Pg[0]));
+	CLA iCLA1(.A(A[7:4]), .B(B[7:4]), .Cin(C[1]), .Cout(), .Sum(Sum[7:4]), .Gg(Gg[1]), .Pg(Pg[1]));
+	CLA iCLA2(.A(A[11:8]), .B(B[11:8]), .Cin(C[2]), .Cout(), .Sum(Sum[11:8]), .Gg(Gg[2]), .Pg(Pg[2]));
+	CLA iCLA3(.A(A[15:12]), .B(B[15:12]), .Cin(C[3]), .Cout(), .Sum(Sum[15:12]), .Gg(Gg[3]), .Pg(Pg[3]));
 
 endmodule
